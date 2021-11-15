fe91c6efb4d6c194be1246df37a328254d733c30
Matthias Bussonnier <bussonniermatthias@gmail.com>
2021-11-15 09:55:14 -0800

    init

---
+++ foo.py
@@ -0,0 +1,16 @@
+def doctest_tb_plain():
+    """
+In [18]: xmode plain
+Exception reporting mode: Plain
+
+In [19]: run simpleerr.py
+Traceback (most recent call last):
+    ...line ..., in <module>
+    bar(mode)
+    ...line ..., in bar
+    div0()
+    ...line ..., in div0
+    x/y
+ZeroDivisionError: ...
+    """
+
