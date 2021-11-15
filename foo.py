111bcd9b0a93b767111800adae3acf103251f3f3
Matthias Bussonnier <bussonniermatthias@gmail.com>
2021-11-15 09:56:42 -0800

    bug

--- foo.py
+++ foo.py
@@ -3,15 +3,15 @@ def doctest_tb_plain():
 In [18]: xmode plain
 Exception reporting mode: Plain
 
-In [19]: run simpleerr.py
-Traceback (most recent call last):
-    ...line ..., in <module>
-    bar(mode)
-    ...line ..., in bar
-    div0()
-    ...line ..., in div0
-    x/y
-ZeroDivisionError: ...
+    In [19]: run simpleerr.py
+    Traceback (most recent call last):
+        ...line ..., in <module>
+        bar(mode)
+        ...line ..., in bar
+        div0()
+        ...line ..., in div0
+        x/y
+    ZeroDivisionError: ...
     """
 
 
